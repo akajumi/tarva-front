093156b0998f6376b24da973e3a710202c6b4f1c
@akaJumi <imazjm@gmail.com>
2018-04-01 21:21:25 +0200

    New project create button text.

--- src/modules/NewProject/NewProject.jsx
+++ src/modules/NewProject/NewProject.jsx
@@ -214,7 +214,7 @@ class NewProject extends Component {
                 </div>
                 <div className="row">
                   <div className="col sm-6 vtr-no-padding">
-                    <button className="btn-small" onClick={this.handleSave}>
+                    <button className="btn-success btn-small" onClick={this.handleSave}>
                       Create project
                     </button>
                   </div>
